879e134f3cacf7180884509c28ef7dac58214212
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2020-03-27 16:46:48 +0200

    Start

--- Project Εξόρυξη Δεδομένων και Αλγόριθμοι Μάθησης/HelperMethods.py
+++ Project Εξόρυξη Δεδομένων και Αλγόριθμοι Μάθησης/HelperMethods.py
@@ -1,7 +1,7 @@
 import csv
 import logging
-
 import WineQualityMetrics
+
 """
 Returns a list that contain the imported, from a csv file rows, as class instances
 
@@ -43,14 +43,23 @@ def CsvImporter(fileName, delimiter, ignoredRows, ignoredColumns):
                     # For every deselected column
                     for csvFileColumn in ignoredColumns:
 
-                        # Set the property associated to the ignored column
+                        # Set the property associated to the ignored column to None
                         csvFileRow[csvFileColumn] = None
 
+                    # Create the class instance based on the imported row
                     classInstance = WineQualityMetrics.WineQualityMetrics(csvFileRow[0], csvFileRow[1], csvFileRow[2], csvFileRow[3], csvFileRow[4], csvFileRow[5], csvFileRow[6], csvFileRow[7],  csvFileRow[8], csvFileRow[9], csvFileRow[10], csvFileRow[11])
+
+                    # Add the class instance to the list
                     instanceList.append(classInstance)
 
                 # Increase the csv line index
                 csvLineIndex = csvLineIndex + 1
+
+            # Return the imported class instances
             return instanceList
+
+    # Catch the IOError
     except IOError:
+
+        # Inform the user
         logging.exception("Csv file cannot be opened.")
--- Project Εξόρυξη Δεδομένων και Αλγόριθμοι Μάθησης/Main.py
+++ Project Εξόρυξη Δεδομένων και Αλγόριθμοι Μάθησης/Main.py
@@ -1,6 +1,24 @@
-import sklearn
+import random
 import HelperMethods
+import sklearn
 
+# Initialise the list that contains the rows that will be ignored during the import
 ignoredRowsList = [0]
-ignoredColumnsList = [0]
-HelperMethods.CsvImporter("Project Εξόρυξη Δεδομένων και Αλγόριθμοι Μάθησης/winequality-red.csv", ",", ignoredRowsList, ignoredColumnsList)
+
+# Initialise the list that contains the columns that will be ignored during the import
+ignoredColumnsList = []
+
+# Import the wine quality metrics from the csv file
+wineQualityMetricsInstanceList = HelperMethods.CsvImporter("Project Εξόρυξη Δεδομένων και Αλγόριθμοι Μάθησης/winequality-red.csv", ",", ignoredRowsList, ignoredColumnsList)
+
+# Get the imported instances count
+importedInstancesSize = len(wineQualityMetricsInstanceList)
+
+# Calculate the size of the test sample
+testSampleLength = round(importedInstancesSize / 4)
+
+# Calculate the size of the test sample
+trainingSampleLength = importedInstancesSize - testSampleLength
+
+# Get the test sample from all the instances
+testSample = random.sample(wineQualityMetricsInstanceList, testSampleLength)
