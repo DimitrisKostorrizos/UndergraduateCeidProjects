b2c6cd328547742ad1bd619d250a7d21f193d46d
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2020-03-27 11:27:07 +0200

    Start

--- Project Εξόρυξη Δεδομένων και Αλγόριθμοι Μάθησης/HelperMethods.py
+++ Project Εξόρυξη Δεδομένων και Αλγόριθμοι Μάθησης/HelperMethods.py
@@ -1,11 +1,24 @@
 import csv
 
+# Returns a list that contain the imported class instances
+def CsvImporter(fileName, delimiter, importingClass, ignoredRows, ignoredColumns):
 
-def CsvImporter(fileName, delimiter, importingClass):
     # Try to open the csv file
     with open(fileName) as csvFile:
-        # Create a csv file reader to read the opened file
-        csvFileReader = csv.reader(csvFile, delimiter=delimiter)
 
+        # Create a csv file reader to read the opened file
+        csvFileReader = csv.reader(csvFile, delimiter = delimiter)
+
+        # Initialize a index for the csv file lines
+        csvLineIndex = 0
+
+        # For each row in the csv file...
+        for csvFileRow in csvFileReader:
+
+            # If the row has to be imported...
+            if csvLineIndex not in ignoredRows:
+                print(csvFileRow)
+
+            csvLineIndex = csvLineIndex + 1
     # Return None if an error occurred
     return None
--- Project Εξόρυξη Δεδομένων και Αλγόριθμοι Μάθησης/Main.py
+++ Project Εξόρυξη Δεδομένων και Αλγόριθμοι Μάθησης/Main.py
@@ -1,3 +1,7 @@
-import sklearn
+from typing import List
 
-print("hello")
+import sklearn
+import HelperMethods
+
+ignoredRowsList = [0]
+HelperMethods.CsvImporter("Project Εξόρυξη Δεδομένων και Αλγόριθμοι Μάθησης/winequality-red.csv", ",", 0, ignoredRowsList, None)
