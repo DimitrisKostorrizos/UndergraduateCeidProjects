57f274f37f36bd72c3b11f73f02cd4209d0c8e6b
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2020-08-12 10:21:52 +0300

    Create JTableColumnClass.java

---
+++ Project Βάσεις Δεδομένων 1/Main/Recruiter_Gui/JTableColumnClass.java
@@ -0,0 +1,166 @@
+package Classes.Swing.Main.Recruiter_Gui;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.table.*;
+
+public class JTableColumnClass extends AbstractCellEditor
+        implements TableCellRenderer, TableCellEditor, ActionListener, MouseListener
+{
+    private JTable table;
+    private Action action;
+    private int mnemonic;
+    private Border originalBorder;
+    private Border focusBorder;
+
+    private JButton renderButton;
+    private JButton editButton;
+    private Object editorValue;
+    private boolean isButtonColumnEditor;
+
+    JTableColumnClass(JTable table, Action action, int column)
+    {
+        this.table = table;
+        this.action = action;
+
+        renderButton = new JButton();
+        editButton = new JButton();
+        editButton.setFocusPainted( false );
+        editButton.addActionListener( this );
+        originalBorder = editButton.getBorder();
+        setFocusBorder( new LineBorder(Color.BLUE) );
+
+        TableColumnModel columnModel = table.getColumnModel();
+        columnModel.getColumn(column).setCellRenderer( this );
+        columnModel.getColumn(column).setCellEditor( this );
+        table.addMouseListener( this );
+    }
+
+    public Border getFocusBorder()
+    {
+        return focusBorder;
+    }
+
+    private void setFocusBorder(Border focusBorder)
+    {
+        this.focusBorder = focusBorder;
+        editButton.setBorder( focusBorder );
+    }
+
+    public int getMnemonic()
+    {
+        return mnemonic;
+    }
+
+    public void setMnemonic(int mnemonic)
+    {
+        this.mnemonic = mnemonic;
+        renderButton.setMnemonic(mnemonic);
+        editButton.setMnemonic(mnemonic);
+    }
+
+    @Override
+    public Component getTableCellEditorComponent(
+            JTable table, Object value, boolean isSelected, int row, int column)
+    {
+        if (value == null)
+        {
+            editButton.setText( "" );
+            editButton.setIcon( null );
+        }
+        else if (value instanceof Icon)
+        {
+            editButton.setText( "" );
+            editButton.setIcon( (Icon)value );
+        }
+        else
+        {
+            editButton.setText( value.toString() );
+            editButton.setIcon( null );
+        }
+
+        this.editorValue = value;
+        return editButton;
+    }
+
+    @Override
+    public Object getCellEditorValue()
+    {
+        return editorValue;
+    }
+
+    public Component getTableCellRendererComponent(
+            JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column)
+    {
+        if (isSelected)
+        {
+            renderButton.setForeground(table.getSelectionForeground());
+            renderButton.setBackground(table.getSelectionBackground());
+        }
+        else
+        {
+            renderButton.setForeground(table.getForeground());
+            renderButton.setBackground(UIManager.getColor("Button.background"));
+        }
+
+        if (hasFocus)
+        {
+            renderButton.setBorder( focusBorder );
+        }
+        else
+        {
+            renderButton.setBorder( originalBorder );
+        }
+        if (value == null)
+        {
+            renderButton.setText( "" );
+            renderButton.setIcon( null );
+        }
+        else if (value instanceof Icon)
+        {
+            renderButton.setText( "" );
+            renderButton.setIcon( (Icon)value );
+        }
+        else
+        {
+            renderButton.setText( value.toString() );
+            renderButton.setIcon( null );
+        }
+
+        return renderButton;
+    }
+
+    public void actionPerformed(ActionEvent e)
+    {
+        int row = table.convertRowIndexToModel( table.getEditingRow() );
+        fireEditingStopped();
+
+        ActionEvent event = new ActionEvent(
+                table,
+                ActionEvent.ACTION_PERFORMED,
+                "" + row);
+        action.actionPerformed(event);
+    }
+
+    public void mousePressed(MouseEvent e)
+    {
+        if (table.isEditing()
+                &&  table.getCellEditor() == this)
+            isButtonColumnEditor = true;
+    }
+
+    public void mouseReleased(MouseEvent e)
+    {
+        if (isButtonColumnEditor
+                &&  table.isEditing())
+            table.getCellEditor().stopCellEditing();
+
+        isButtonColumnEditor = false;
+    }
+
+    public void mouseClicked(MouseEvent e) {}
+    public void mouseEntered(MouseEvent e) {}
+    public void mouseExited(MouseEvent e) {}
+}
