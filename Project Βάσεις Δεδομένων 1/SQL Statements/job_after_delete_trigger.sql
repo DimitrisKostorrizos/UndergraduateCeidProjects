104dc5c81edc59b121bf402b100a8e14e7394841
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2020-08-17 16:39:49 +0300

    Create job_after_delete_trigger.sql

---
+++ Project Βάσεις Δεδομένων 1/SQL Statements/job_after_delete_trigger.sql
@@ -0,0 +1,5 @@
+CREATE DEFINER=`root`@`localhost` TRIGGER `job_AFTER_DELETE` AFTER DELETE ON `job` FOR EACH ROW BEGIN
+	declare username varchar(12);
+	select user into username from login order by date desc limit 1;
+	insert into record values (username, now(), 'Success', 'delete', 'job');
+END
