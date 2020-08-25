3a768bea93d8e125420d157ac43a50fbcecbe9c8
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2020-08-25 20:45:22 +0300

    FortyPercent

--- Project Web/WebServer/WebServer.js
+++ Project Web/WebServer/WebServer.js
@@ -7,29 +7,55 @@ var urlModule = require('url');
 // Get the querystring module
 var querystringModule = require('querystring');
 
-// Set the localhost
+// Set the host name
 const host = 'localhost';
 
 // Set the web server port
 const port = 8080;
 
-const urlParser = function (requestObject, responseObject) 
+// The server listening function
+const webServerListeningFunction = function (requestObject, responseObject)
 {
+  // Set the response code to 200
   responseObject.statusCode = 200;
-  urlString = urlModule.parse(requestObject.url, true);
+
+  // Parse the url
+  var parsedUrlObject = urlParserFunction(requestObject.url);
+
+  // Get the selected endpoint
+  var selectedEndpoint = endpointMapperFunction(parsedUrlObject);
+
+  responseObject = endpointResponse();
+};
+
+// The url parser function
+const urlParserFunction = function (url) 
+{
+  // Parse the url
+  return urlObject = urlModule.parse(url, true);
+};
+
+// The server endpoint mapper
+const endpointMapperFunction = function (urlObject)
+{
+  // Get the pathname
+  var pathname = urlObject.pathname;
+
+  // Get the query arguments
+  var queryArguments = urlObject.query;
+};
+
+// The server endpoint response
+const endpointResponse = function ()
+{
+
 };
 
 // Create the server
-var webServer = httpModule.createServer(urlParser);
-
-// Get the pathname
-//var pathname = urlString.pathname;
-
-// Get the query arguments
-//var queryArguments = urlString.query;
+var webServer = httpModule.createServer(webServerListeningFunction);
 
 // Set the server listening port
 webServer.listen(port, host, () => 
 {
-  console.log('Server is running on http://${host}:${port}');
+  console.log(`Server is running on http://${host}:${port}`);
 });
