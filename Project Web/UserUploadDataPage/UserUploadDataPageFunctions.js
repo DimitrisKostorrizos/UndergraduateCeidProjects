802320064343b618b5d40a88db02be201de786aa
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2020-09-16 15:53:10 +0300

    Updates

--- Project Web/UserUploadDataPage/UserUploadDataPageFunctions.js
+++ Project Web/UserUploadDataPage/UserUploadDataPageFunctions.js
@@ -1,18 +1,69 @@
+// Initialize the GUI
+function Initialization()
+{
+    // Hide the input
+    HideElement("UploadButton");
+
+    // Show the text box
+    HideElement("ChosenFilename");
+
+    // Show the text box
+    HideElement("MapContainer");
+}
+
+// Open the file explorer
 function OpenFileExplorer() 
 {
     // Click the collapsed input
     document.getElementById("fileInput").click();
 }
 
+// Open the file and set the map
 async function OpenFile(file)
 {
-    // Read the loacl file asynchronously
+    // Read the local file asynchronously
     var fileContext = await ReadFileAsync(file);
 
     // Parse the file context
     var JSONObject = JSON.parse(fileContext);
 
-    MapSetter(MapId, JSONObject);
+    // Hide the input
+    HideElement("JSONFileInput");
+
+    // Show the text box
+    ShowElement("ChosenFilename");
+
+    // Show the text box
+    ShowElement("MapContainer");
+
+    // Show the input
+    ShowElement("UploadButton");
+
+    // Set the file name
+    document.getElementById("fileNameValue").innerHTML = file.name;
+
+    // Set the map
+    MapSetter("MapContainer", JSONObject);
+}
+
+// Hide the element
+function HideElement(elementId)
+{
+    // Get the element by id
+    var element = document.getElementById(elementId);
+
+    // Hide the element
+    element.style.display = "none"; 
+}
+
+// Show the element
+function ShowElement(elementId)
+{
+    // Get the element by id
+    var element = document.getElementById(elementId);
+
+    // Show the element
+    element.style.display = "block";     
 }
 
 // Read the file asynchronously
@@ -25,31 +76,83 @@ async function ReadFileAsync(file)
     return text;
 }
 
+// Set the map
 function MapSetter(MapId, JSONObject)
 {
-    let CenterCoordinates = [38.230462, 21.753150];
+    // Initialize the coordinates for patra center
+    var CenterCoordinates = [38.230462, 21.753150];
+
+    // Initialize the map layer
     var map = L.map(MapId, { drawControl: true }).setView(CenterCoordinates, 14);
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
+
+    // Initialize a map layer
+    var mapLayer = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    var drawnItems = new L.FeatureGroup();
-    map.addLayer(drawnItems);
-
-    drawControl = new L.Control.Draw({
-        draw : {
-            position : 'topright',
-            polyline : false,
-            rectangle : false,
-            circle : false,
-            polygon: false,
-            marker: false
-        },
-        edit : {
-            featureGroup: drawnItems
-          }
     });
+
+    // Add the map layer to the map
+    map.addLayer(mapLayer);
+
+    // Initialize a group for the editable layers
+    var featureGroupLayer = new L.FeatureGroup();
+
+    // Add the feature layer to the map
+    map.addLayer(featureGroupLayer);
+
+    // Intialize the draw plugin options
+    var drawPluginOptions = 
+    {
+        position: 'topright',
+        draw: {
+            polyline: {
+            shapeOptions: {
+                color: '#f357a1',
+                weight: 10
+            }
+            },
+            polygon: {
+            allowIntersection: false, // Restricts shapes to simple polygons
+            drawError: {
+                color: '#e1e100', // Color the shape will turn when intersects
+                message: '<strong>Polygon draw does not allow intersections!<strong> (allowIntersection: false)' // Message that will show when intersect
+            },
+            shapeOptions: {
+                color: '#bada55'
+            }
+            },
+            circle: false, // Turns off this drawing tool
+            rectangle: {
+            shapeOptions: {
+                clickable: false
+            }
+            }
+        },
+        edit: {
+            featureGroup: featureGroupLayer,
+            remove: false
+        }
+    };
+
+    // Initialize the draw control
+    var drawControl = new L.Control.Draw(drawPluginOptions);
+
+    // Add the draw control to the map
     map.addControl(drawControl);
+
+    // drawControl = new L.Control.Draw({
+    //     draw : {
+    //         position : 'topright',
+    //         polyline : false,
+    //         rectangle : false,
+    //         circle : false,
+    //         polygon: false,
+    //         marker: false
+    //     },
+    //     edit : {
+    //         featureGroup: drawnItems
+    //       }
+    // });
+    // map.addControl(drawControl);
 
     // map.on(L.Draw.Event.CREATED, function (e) {
     //     var layer = e.layer;
