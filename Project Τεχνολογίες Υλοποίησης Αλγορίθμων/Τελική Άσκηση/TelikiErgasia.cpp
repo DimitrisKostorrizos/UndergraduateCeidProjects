3021c5311eb48b844a9d03b71b0296fc881e82a3
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2020-09-27 20:26:27 +0300

    Update TelikiErgasia.cpp

--- Project Τεχνολογίες Υλοποίησης Αλγορίθμων/Τελική Άσκηση/TelikiErgasia.cpp
+++ Project Τεχνολογίες Υλοποίησης Αλγορίθμων/Τελική Άσκηση/TelikiErgasia.cpp
@@ -4,6 +4,7 @@
 #include <boost/graph/random.hpp>
 #include <LEDA/graph/graph.h>
 
+
 //#include <LEDA/graph/shortest_path.h>
 
 //export LEDAROOT=/home/jim/Project/Leda/LEDA-6.3-free-fedora-core-8-64-g++-4.1.2; export LD_LIBRARY_PATH=$LEDAROOT
@@ -42,17 +43,19 @@ inline double Heuristic()
   return 5;
 }
 
-void AStarSearch(DirectedGraph& directedGraph, Vertex startingVertex, Vertex targetVertex)
+/**
+ * 
+ * @param
+ * @param
+ * @param
+ */
+bool AStarSearch(DirectedGraph& directedGraph, Vertex startingVertex, Vertex targetVertex)
 {
 	// Initialize the property map that contain the edges's weights
 	EdgeWeightMap boostEdgeWeightMap = get(edge_weight, directedGraph);
 
-	std::map<Vertex, double> currentPathCostMap;
-
-	std::map<Vertex, Vertex> pathVertexesMap;
-
-	// Set the initial vertex
-	pathVertexesMap[startingVertex] = startingVertex;
+	// Declare a map for the the vertexes and the cost to traverse them
+	std::map<Vertex, double> outgoingPathCostMap;
 
 	// Initialise the boost vertex iterators
 	VertexIterator vertexIteratorBegin, vertexIteratorEnd;
@@ -61,22 +64,22 @@ void AStarSearch(DirectedGraph& directedGraph, Vertex startingVertex, Vertex tar
 	OutEdgeIterator edgeIteratorBegin, edgeIteratorEnd;
 
 	// Set the initial vertex
-	Vertex currentVertex = startingVertex;
+	Vertex lastVisitedVertex = startingVertex;
 
 	// Initialize the path cost
-	double pathCost = 0;
+	double totalPathCost = 0;
 
 	// While there is a vertex to search...
-	while (currentVertex != 0) 
+	while (lastVisitedVertex != 0) 
 	{
 		// Clear the cost map
-		currentPathCostMap.clear();
+		outgoingPathCostMap.clear();
 
 		// If the target node was reached...
-		if (currentVertex == targetVertex) 
+		if (lastVisitedVertex == targetVertex) 
 		{
-			cout << "Target found." << endl;
-			break;
+			// Return true
+			return true;
 		}
 		
 		// For every out edge of the current vertex... 
@@ -89,30 +92,43 @@ void AStarSearch(DirectedGraph& directedGraph, Vertex startingVertex, Vertex tar
 			double edgeWeight = boostEdgeWeightMap[*edgeIteratorBegin];
 
 			// Calculate the new cost
-			double newCost = pathCost + edgeWeight + Heuristic();
+			double newCost = totalPathCost + edgeWeight + Heuristic();
 
 			// Set the cost to the edge's target
-			currentPathCostMap[currentEdgeTargetVertex] = newCost;
+			outgoingPathCostMap[currentEdgeTargetVertex] = newCost;
 		}
 
-		double minimumCost = INT_MAX;
+		// Set the minimum cost to maximum double
+		double minimumCost = DBL_MAX;
 
+		// Declare the vertex with the minimum cost
 		Vertex minimumCostVertex = 0;
 
-		for (std::map<Vertex, double>::iterator mapIterator=currentPathCostMap.begin(); mapIterator!=currentPathCostMap.end(); ++mapIterator)
+		// For every selected vertex...
+		for (std::map<Vertex, double>::iterator mapIterator = outgoingPathCostMap.begin(); mapIterator!=outgoingPathCostMap.end(); ++mapIterator)
 		{
+			// If the vertex travel cost is less the the previous minimum cost...
 			if(mapIterator->second < minimumCost)
 			{
+				// Set the vertex as the new vertex
 				minimumCostVertex = mapIterator->first;
 
+				// Set the vertex's costs as the minimum cost
 				minimumCost = mapIterator->second;
 			}
 		}
 
-		currentVertex = minimumCostVertex;
+		// Set the last visited vertex
+		lastVisitedVertex = minimumCostVertex;
 
-		pathCost += minimumCost;
+		// Add the traversal cost to the path cost
+		totalPathCost += minimumCost;
 	}
+
+	cout << "Traversed path total cost: " << totalPathCost << endl;
+
+	// Return false
+	return false;
 }
 
 /**
@@ -262,10 +278,16 @@ int main()
 	}
 
 	// Choose a random node from the Leda directed graph
-	node randomLedaNode = ledaDirectedGraph.choose_node();
+	node startingRandomLedaNode = ledaDirectedGraph.choose_node();
+
+	// Choose a random node from the Leda directed graph
+	node targetRandomLedaNode = ledaDirectedGraph.choose_node();
 
 	// Get the index of the Boost directed graph vertex that correspond to the appropriate Leda random chosen node
-	Vertex randomBoostVertex = vertex(ledaDirectedGraph.index(randomLedaNode), boostDirectedGraph);
+	Vertex startingRandomBoostVertex = vertex(ledaDirectedGraph.index(startingRandomLedaNode), boostDirectedGraph);
+
+	// Get the index of the Boost directed graph vertex that correspond to the appropriate Leda random chosen node
+	Vertex targetRandomBoostVertex = vertex(ledaDirectedGraph.index(startingRandomLedaNode), boostDirectedGraph);
 	
 	// Initialise a node array the will contain the last edge on a shortest path from the starting node to a node
 	node_array<leda::edge> ledaPredecessorNodeArray(ledaDirectedGraph);
@@ -279,18 +301,8 @@ int main()
 	// Initialize a vector that will contain the distance to each node and set the initial distance to INT_MAX
 	std::vector<int> boostDistanceVector(numberOfNodes,INT_MAX);
 
-	// Set the distance of the chosen vertex to 0
-	boostDistanceVector[randomBoostVertex] = 0;
-
 	// Initialize a vector that will contain the predeccesor of each node
 	std::vector<std::size_t> boostPredeccesorVector(numberOfNodes);
-
-	// For every vertex in the boost directed graph...
-	for(int index = 0; index < numberOfNodes; index++)
-	{
-		// Set the current vertex predeccesor to itself
-		boostPredeccesorVector[index] = index;
-	}
 	
 	#pragma endregion Initialization
 
@@ -299,7 +311,14 @@ int main()
 	// Initialise the starting CPU time
 	float CPUTime = used_time();
 
-	AStarSearch(boostDirectedGraph, randomBoostVertex, randomBoostVertex);
+	// Execute user defined ALT function
+	bool targetVertexFound = AStarSearch(boostDirectedGraph, startingRandomBoostVertex, targetRandomBoostVertex);
+
+	// If the target was found...
+	if(targetVertexFound)
+		cout << "Target found." << endl;
+	else
+		cout << "Target not found." << endl;
 
 	// Print the user defined Bellman Ford function execution time
 	cout << "User defined A* function execution time: " << used_time(CPUTime) << " seconds."<< endl;
