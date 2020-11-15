ae18baad4f017e5d2e706e685c2ba68427f449e2
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2020-11-15 02:07:31 +0200

    Create RunMe.java

---
+++ Project Οντοκεντρικός Προγραμματισμός/Set Ασκήσεων/Set1 Java/Μέρος Β/RunMe.java
@@ -0,0 +1,12 @@
+public class RunMe
+{
+public static void main(String args[])
+{
+Box b1=new Box(2.5,3.0,4.0);
+MyBox mb1=new MyBox(3.0,3.0,3.0);
+Spirtokouto sp1=new Spirtokouto(2.0,4.0,3.0,10.0);
+System.out.println(b1.toString());
+System.out.println(mb1.toString());
+System.out.println(sp1.toString());
+}
+}
