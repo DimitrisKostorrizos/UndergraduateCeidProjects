0d401032a08c3d22135e08b34be234fc6fbe1940
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2020-11-24 10:59:19 +0200

    Create Room.h

---
+++ Project Οντοκεντρικός Προγραμματισμός/C++ Project/Visual Studio Hotel Project/ConsoleApplication1/Room.h
@@ -0,0 +1,37 @@
+#ifndef ROOM_H
+#define ROOM_H
+
+class Booking;//Forward Declaration.
+
+class Room
+{
+public:
+	Room();
+	Room(int pricePerPerson);
+	virtual ~Room();
+
+	double getPricePerPerson() { return pricePerPerson; }
+	void setPricePerPerson(int val) { pricePerPerson = val; }
+	int getMaxPeople() { return maxPeople; }
+	void setMaxPeople(int val) { maxPeople = val; }
+	void setRoomID() { roomID = ++roomInstanceCounter; }
+	int getRoomID() { return roomID; }
+
+	Booking* getRoomAvailability(int i) { return *(roomAvailability + i); }
+
+	virtual bool addBooking(Booking*);
+
+	virtual double getPriceOfRoom();
+	bool cancelBooking(int);
+	double checkFullness();
+
+protected:
+	Booking *roomAvailability[31] = {};
+
+private:
+	int pricePerPerson;
+	int maxPeople;
+	int roomID;
+	static int roomInstanceCounter;
+};
+#endif // ROOM_H
