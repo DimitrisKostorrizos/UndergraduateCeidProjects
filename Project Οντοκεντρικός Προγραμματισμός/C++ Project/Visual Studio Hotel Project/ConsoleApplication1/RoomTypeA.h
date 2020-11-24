139014efc6c51ada301898119d3f150dc054a16f
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2020-11-24 10:59:12 +0200

    Create RoomTypeA.h

---
+++ Project Οντοκεντρικός Προγραμματισμός/C++ Project/Visual Studio Hotel Project/ConsoleApplication1/RoomTypeA.h
@@ -0,0 +1,22 @@
+#ifndef ROOMTYPEA_H
+#define ROOMTYPEA_H
+
+#include "Room.h"
+
+
+class RoomTypeA : public Room
+{
+public:
+	RoomTypeA();
+	RoomTypeA(double pricePerDay, int maxPeople);
+	virtual ~RoomTypeA();
+
+	double getPricePerDay() { return pricePerDay; }
+	void setPricePerDay(double val) { pricePerDay = val; }
+
+	double getPriceOfRoom();
+private:
+	double pricePerDay;
+};
+
+#endif // ROOMTYPEA_H
