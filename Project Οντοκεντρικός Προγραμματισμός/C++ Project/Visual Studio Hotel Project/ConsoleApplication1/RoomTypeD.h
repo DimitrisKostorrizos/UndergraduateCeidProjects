ff2d911032d6aba45349436ef57e5c9a3ed913f9
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2020-11-24 10:48:18 +0200

    Create RoomTypeD.h

---
+++ Project Οντοκεντρικός Προγραμματισμός/C++ Project/Visual Studio Hotel Project/ConsoleApplication1/RoomTypeD.h
@@ -0,0 +1,24 @@
+#ifndef ROOMTYPED_H
+#define ROOMTYPED_H
+
+#include "Room.h"
+#include "Booking.h"
+
+
+class RoomTypeD : public Room
+{
+public:
+	RoomTypeD(double priceOfRoom, int maxPeople);
+	virtual ~RoomTypeD();
+
+	double getPriceOfRoom();
+	void setPriceOfRoom(double price)
+	{
+		priceOfRoom = price;
+	}
+	bool cancelBooking(int);
+	bool addBooking(Booking* a);
+private:
+	double priceOfRoom;
+};
+#endif // ROOMTYPED_H
