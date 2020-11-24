8900dd0ef3644b7be4b1b4f3344bba3890631e62
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2020-11-24 10:58:29 +0200

    Create RoomTypeC.h

---
+++ Project Οντοκεντρικός Προγραμματισμός/C++ Project/Visual Studio Hotel Project/ConsoleApplication1/RoomTypeC.h
@@ -0,0 +1,25 @@
+#ifndef ROOMTYPEC_H
+#define ROOMTYPEC_H
+
+#include "Room.h"
+
+
+class RoomTypeC : public Room
+{
+public:
+	RoomTypeC();
+	RoomTypeC(int minPeople, int minDays, int maxPeople, double pricePerPerson);
+	virtual ~RoomTypeC();
+
+	int getMinPeople() { return minPeople; }
+	void setMinPeople(int val) { minPeople = val; }
+	int getMinDays() { return minDays; }
+	void setMinDays(int val) { minDays = val; }
+
+	bool addBooking(Booking*);
+
+private:
+	int minPeople;
+	int minDays;
+};
+#endif // ROOMTYPEC_H
