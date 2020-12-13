4670cfeb06d6bef258184d511a17eb585f1415e6
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2020-12-13 15:25:27 +0200

    Create Constants.cs

---
+++ Project Αναπαράσταση Γνώσης στον Παγκόσμιο Ιστό/1η Άσκηση/Ερώτημα 12/AnaparastasiGnosis/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace AnaparastasiGnosis
+{
+    /// <summary>
+    /// Constants
+    /// </summary>
+    public static class Constants
+    {
+        public const string File = "./schedule.xml";
+    }
+}
