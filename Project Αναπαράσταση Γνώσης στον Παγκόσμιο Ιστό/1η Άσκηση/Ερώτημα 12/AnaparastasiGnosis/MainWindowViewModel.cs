820976e50c6f35b090bef543b3ae4794f380fd88
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2020-12-15 21:06:23 +0200

    Create MainWindowViewModel.cs

---
+++ Project Αναπαράσταση Γνώσης στον Παγκόσμιο Ιστό/1η Άσκηση/Ερώτημα 12/AnaparastasiGnosis/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Atom.Windows.Controls;
+
+using System.Windows;
+
+namespace AnaparastasiGnosis
+{
+    /// <summary>
+    /// The main window view model
+    /// </summary>
+    public class MainWindowViewModel : WindowViewModel
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public MainWindowViewModel(Window window) : base(window) => IsMain = true;
+
+        #endregion
+    }
+}
