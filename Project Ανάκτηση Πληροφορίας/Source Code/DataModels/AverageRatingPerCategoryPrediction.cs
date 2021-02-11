87b8eb89d296234e96c63515a1f294ffd5472368
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2021-02-11 09:42:39 +0200

    Create AverageRatingPerCategoryPrediction.cs

---
+++ Project Ανάκτηση Πληροφορίας/Source Code/DataModels/AverageRatingPerCategoryPrediction.cs
@@ -0,0 +1,29 @@
+﻿namespace Anaktisi
+{
+    /// <summary>
+    /// The average rating per category prediction
+    /// </summary>
+    public class AverageRatingPerCategoryPrediction : AverageRatingPerCategory
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Predicted cluster label from the trainer
+        /// </summary>
+        public uint PredictedLabel { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public AverageRatingPerCategoryPrediction()
+        {
+
+        }
+
+        #endregion
+    }
+}
