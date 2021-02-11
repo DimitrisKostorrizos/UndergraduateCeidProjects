f8010c57272c26b9aa6b31480f18fc3832c62e98
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2021-02-11 09:42:45 +0200

    Create GenresOneHotEncodingDataModel.cs

---
+++ Project Ανάκτηση Πληροφορίας/Source Code/DataModels/GenresOneHotEncodingDataModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Anaktisi
+{
+    /// <summary>
+    /// The movie genres one hot encoding data model
+    /// </summary>
+    public class GenresOneHotEncodingDataModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The aggregated, using comma, genres
+        /// </summary>
+        public string AggregatedGenres { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public GenresOneHotEncodingDataModel()
+        {
+
+        }
+
+        #endregion
+    }
+}
