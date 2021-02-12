83b93cddd4bed8e77b31efd4bbdbeba94b500b17
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2021-02-12 10:01:16 +0200

    Create TitleWordEmbeddingsDataModel.cs

---
+++ Project Ανάκτηση Πληροφορίας/Source Code/DataModels/TitleWordEmbeddingsDataModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Anaktisi
+{
+    /// <summary>
+    /// The movie title word embedding data model
+    /// </summary>
+    public class TitleWordEmbeddingsDataModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The movie title
+        /// </summary>
+        public string Title { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public TitleWordEmbeddingsDataModel()
+        {
+
+        }
+
+        #endregion
+    }
+}
