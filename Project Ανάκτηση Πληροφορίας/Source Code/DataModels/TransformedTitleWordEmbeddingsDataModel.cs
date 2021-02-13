1bd3d3271f3fcbfdc6ecc9b128fc758266c6a51a
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2021-02-13 19:53:55 +0200

    Create TransformedTitleWordEmbeddingsDataModel.cs

---
+++ Project Ανάκτηση Πληροφορίας/Source Code/DataModels/TransformedTitleWordEmbeddingsDataModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Anaktisi
+{
+    /// <summary>
+    /// The transformed movie title word embedding data model
+    /// </summary>
+    public class TransformedTitleWordEmbeddingsDataModel : TitleWordEmbeddingsDataModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The word embeddings
+        /// </summary>
+        public float[] WordEmbeddings { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public TransformedTitleWordEmbeddingsDataModel()
+        {
+
+        }
+
+        #endregion
+    }
+}
