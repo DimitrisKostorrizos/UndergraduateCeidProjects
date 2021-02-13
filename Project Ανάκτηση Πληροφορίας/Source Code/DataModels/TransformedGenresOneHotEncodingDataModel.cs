e72debaccf61b04e7475542cc8d123f69ef5016a
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2021-02-13 19:53:49 +0200

    Create TransformedGenresOneHotEncodingDataModel.cs

---
+++ Project Ανάκτηση Πληροφορίας/Source Code/DataModels/TransformedGenresOneHotEncodingDataModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Anaktisi
+{
+    /// <summary>
+    /// The transformed movie genres one hot encoding data model
+    /// </summary>
+    public class TransformedGenresOneHotEncodingDataModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The one hot encoding transformation
+        /// </summary>
+        public float[] AggregatedGenres { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public TransformedGenresOneHotEncodingDataModel()
+        {
+
+        }
+
+        #endregion
+    }
+}
