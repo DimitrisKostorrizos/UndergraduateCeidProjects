8af584181c48092364846ca9bf717c9a8a496d08
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2021-02-18 09:22:46 +0200

    Create UserRatingPerCategory.cs

---
+++ Project Ανάκτηση Πληροφορίας/Source Code/DataModels/UserRatingPerCategory.cs
@@ -0,0 +1,34 @@
+﻿namespace Anaktisi
+{
+    /// <summary>
+    /// The category ratings per user
+    /// </summary>
+    public class UserRatingPerCategory
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The user id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The rating per movie category
+        /// </summary>
+        public AverageRatingPerCategory RatingsPerCategory { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public UserRatingPerCategory()
+        {
+
+        }
+
+        #endregion
+    }
+}
