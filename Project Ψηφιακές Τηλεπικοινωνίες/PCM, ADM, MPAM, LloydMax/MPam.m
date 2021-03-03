b421a919bd674d43ddf99c37aa0d6a9e85dc3943
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2021-03-03 20:12:07 +0200

    Create MPam.m

---
+++ Project Ψηφιακές Τηλεπικοινωνίες/PCM, ADM, MPAM, LloydMax/MPam.m
@@ -0,0 +1,162 @@
+function [OutputBitMatrix, OutputSymbolVector, InputSymbolVector, InputBitMatrix] = MPam(N, M, EncodingType, SNR)
+    
+    % Get the bits per symbol
+    BitPerSymbol = ceil(log2(M));
+    
+    % Get the number of symbols
+    NumberOfSymbols = ceil(N/BitPerSymbol);
+    
+    % Generate a matrix that contains the bit per symbol
+    % Row: Symbol
+    % Columns: Bits that represent the symbol
+    BitMatrix = randsrc(NumberOfSymbols, BitPerSymbol,[0 1]);
+    
+    % Return the BitMatrix
+    InputBitMatrix = BitMatrix;
+    
+    % Encode the binary matrix to decimal vector
+    % Element: Decimal representation of the symbol
+    DecimalVector = bi2de(BitMatrix);
+    
+    % If the encoding type is Gray..
+    if EncodingType == "Gray"
+        % Encode the decimal symbols to Gray decimal symbols
+        SymbolVector = bin2gray(DecimalVector, 'pam', M);
+    else
+        % Use the simple encoded decimal symbols
+        SymbolVector = DecimalVector;
+    end
+    
+    % Set the input symbol vector
+    InputSymbolVector = SymbolVector;
+    
+    % Calculate the symbol period
+    TSymbol = 4*10^(-6);
+    
+    % Calculate the sampling period
+    Ts = 0.1 * 10^(-6);
+    
+    % Calculate the number of samples
+    NumberOfSamples = TSymbol/Ts;
+    
+    % Calculate the frequency
+    Fc = 2.5 * 10 ^ 6;
+    
+    % Calaculate the pulse amplitude
+    Gt = sqrt(2/TSymbol);
+    
+    % Calculate the pulse energy 
+    PulseEnergy = TSymbol * Gt^2;
+    
+    % Initialise a temp variable to 0
+    % Temp: The sum of the energy of the symbols
+    temp = 0;
+    
+    % For every energy level...
+    for index = 1 : M
+        % Add the energy required
+        temp = temp + (2 * index - 1 - M) ^ 2;
+    end
+    
+    % Calculate the average pulse amplitude
+    A = sqrt(M / (PulseEnergy * temp));
+    
+    % Declare a vector that will contain the amplitude per symbol
+    SymbolAmplitude = zeros(M,1); 
+    
+    % For every symbol...
+    for index = 1 : M
+        % Calculate the amplitude per symbol
+        SymbolAmplitude(index) = (2 * index - 1 - M) * A;
+    end
+    
+    % Initialise a counter to 1
+    % counter: The couter points to the position of the modulated signal
+    counter = 1;
+    
+    % Delcare a vector for the modulated signal
+    Sm = zeros(NumberOfSymbols * NumberOfSamples,1);
+    
+    % For every symbol...
+    for index = 1 : NumberOfSymbols
+        
+        % For every sample...
+        for Time = 0 : Ts : TSymbol - Ts
+            
+            % Calculate the modulated signal
+            Sm(counter) = SymbolAmplitude(SymbolVector(index) + 1) * Gt * cos(2 * pi * Fc * Time);
+            
+            % Increase the counter
+            counter = counter + 1;
+        end
+    end
+    
+    % Calculate the variance
+    Variance = ((1/(2*log2(M))) * (10 ^ (-SNR/10)));
+    
+    % Calculate the additive white Gausian noise
+    AWGNoise = sqrt(Variance) * randn(NumberOfSymbols * 40, 1);
+    
+    % Add the additive white Gausian noise to the signal
+    Rn = Sm + AWGNoise;
+    
+    % Initialise a counter to 1
+    % counter: The couter points to the position of the demodulated symbol
+    counter = 1;
+    
+    % Declare a vector for the demodulated symbols
+    Smt = zeros(NumberOfSymbols,1);
+    
+    % For every symbol...
+    for index = 1 : NumberOfSymbols
+        
+        % For every sample...
+        for Time = 0 : Ts : TSymbol - Ts
+            
+            % Calculate the demodulated symbol
+            Smt(index) = Smt(index) + Rn(counter) * Gt * cos(2 * pi * Fc * Time) * Ts;
+            
+            % Increase the counter
+            counter = counter + 1;
+        end
+    end
+
+    % Declare a vector for the symbol distance
+    DistanceVector = zeros(1,M);
+    
+    % Declare a vector for the demapped symbols
+    SymbolVector = zeros(NumberOfSymbols, 1);
+    
+    % For every symbol...
+    for counter = 1 : NumberOfSymbols
+        
+        % For every symbol type...
+        for index = 1 : M
+            
+            % Calculate the absolute distance between the initial symbol
+            % and the demapped symbol
+            DistanceVector(index) = abs(Smt(counter) - SymbolAmplitude(index));
+        end
+        
+        % Get the index that points to the minimum diastance
+        [~,MinimumPosition] = min(DistanceVector);
+        
+        % Get the most similar symbol
+        SymbolVector(counter) = MinimumPosition - 1;
+    end
+    
+    % Set the output symbol vector
+    OutputSymbolVector = SymbolVector;
+    
+    % If the encoding type is Gray...
+    if EncodingType == "Gray"
+        % Decode the gray decimal symbols to decimal symbols
+        DecimalVector = gray2bin(SymbolVector, 'pam', M);
+    else
+        % Use the decimal symbols
+        DecimalVector = SymbolVector;
+    end
+	
+    % Decode the decimal symbols to bits
+    OutputBitMatrix = de2bi(DecimalVector);
+end
